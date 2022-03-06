76dd8a78d04b5afe3c20932d22ed2e6b27fdcfdf
DaliaSir <66668341+DaliaSir@users.noreply.github.com>
2022-03-06 11:27:29 +0100

    Added Content-Types

---
+++ src/api/tools-category/controllers/tools-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  tools-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tools-category.tools-category');
---
+++ src/api/tools-category/routes/tools-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tools-category router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tools-category.tools-category');
